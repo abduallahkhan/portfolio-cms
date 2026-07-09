027257f1a7fecd6383e7b123d1e8297c28e5e274
Abdullah Khan <your@email.com>
2026-07-09 17:12:36 +0500

    Fix auth URL

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AuthContext = createContext();
 
-const API = 'http://localhost:5000/api';
+const API = const API = 'https://portfolio-cms-production-22ff.up.railway.app/api';
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
