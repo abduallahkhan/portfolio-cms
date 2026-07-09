6f9131a6d1d1b7d00b35b8dae05de97f061bdba6
Abdullah Khan <your@email.com>
2026-07-09 13:53:51 +0500

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://peppy-kleicha-fb678f.netlify.app', 'http://localhost:3000'],
+  credentials: true
+}));;
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
